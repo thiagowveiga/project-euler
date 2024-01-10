360c0f33d355e9e72ffef5b45431919bc07bad25
Thiago Veiga <thiagoveiga@Macintosh-8.local>
2024-01-10 08:15:36 -0300

    p36 added mathematical approach

--- p036.py
+++ p036.py
@@ -1,4 +1,5 @@
 import time
+from math import log, ceil
 
 def is_palindrome_str(s):
     i = 0
@@ -10,6 +11,20 @@ def is_palindrome_str(s):
         j -= 1
     return True
 
+def is_palindrome_mat(n, base=10):
+    if n == 1:
+        return True
+    k = n
+    rev = 0
+    i = 1
+    while k > 0:
+        rev = rev*base + k%base
+        k //= base
+        if rev != n//(base**(ceil(log(n, base)) - i)):
+            return False
+        i += 1
+    return True
+
 start = time.time()
 
 sum_palindromic = 0
@@ -19,4 +34,17 @@ for i in range(1, 1_000_000):
 
 end = time.time()
 
-print(sum_palindromic, f'{round(end-start, 2)}s')
+print(f'Sum: {sum_palindromic}')
+print('String approach:', f'({round(end-start, 2)}s)')
+
+start = time.time()
+
+sum_palindromic = 0
+for i in range(1, 1_000_000):
+    if is_palindrome_mat(i, 10) and is_palindrome_mat(i, 2):
+            sum_palindromic += i
+
+end = time.time()
+
+print('Mathematical approach: ', f'({round(end-start, 2)}s)')
+#print(sum_palindromic)

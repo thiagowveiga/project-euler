ab27153a98c3e9c7b691f92265fcdf5367553f53
Thiago Veiga <thiagoveiga@Macintosh-8.local>
2024-01-10 14:19:31 -0300

    p9 numpy way

--- p009.py
+++ p009.py
@@ -5,10 +5,16 @@
 #   b = ((1000**2)/2 - 1000*a)/(1000 - a)
 #   c = 1000 - (a + b)
 
+import time
+import numpy as np
+
 k = 1000
 stop = 500
 a = 1
-while a < 500: 
+
+start = time.time()
+
+while a < 500: # for a > 500, b < 0 - that was pretty smart from old me
     b = ((k**2)/2 - k*a)/(k - a)
     c = k - (a + b)
     soma = a + b + c
@@ -17,3 +23,18 @@ while a < 500:
         print(int(a*b*c))   # here too
         a = stop + 1        # how to break for advanced pro masters!
     a += 1
+
+print(f'first version: ({time.time()-start:4f}s)')
+
+start = time.time()
+
+a = np.arange(1, 500)
+b = (((1000**2)/2 - 1000*a)/(1000 - a))
+c = (1000 - (a + b))
+b = np.where(b%1==0, b, 0)
+c = np.where(c%1==0, c, 0)
+soma = a + b + c
+i = np.where(soma==1000)[0][0]
+print(a[i], b[i], c[i], a[i]*b[i]*c[i])
+
+print(f'new version: ({time.time()-start:4f}s)')
